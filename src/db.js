4ba18c52faa147939c8e37dce8f3ef7a1fc1be0b
Gabriel Souza <gabriel.souza@servier.com>
2024-04-07 12:59:35 -0300

    erwfwefw

--- src/db.js
+++ src/db.js
@@ -4,5 +4,6 @@ const equipamentos = [
     { tipo: "desktop", nome: "Core i3 8GB 500GB", status: "Excelente", preco: "1200,00"},
     { tipo: "desktop", nome: "Core i3 8GB 500GB", status: "Bom", preco: "1000,00"},
     { tipo: "smartphone", nome: "Iphone SE 16GB", status: "Muito bom", preco: "1000,00"},
-    { tipo: "notebook", nome: "Dell Latitude 3400", status: "Excelente", preco: "1500,00"}
+    { tipo: "notebook", nome: "Dell Latitude 3400", status: "Excelente", preco: "1500,00"},
 ];
+
