87b28928064e2a0bf9a1f17fbacdf6795380022f
Gabriel Souza <gabriel.souza@servier.com>
2024-04-08 08:49:46 -0300

    asdfad

--- src/script.js
+++ src/script.js
@@ -15,7 +15,7 @@ function mostrarEquipamentos() {
         elemento.innerHTML = `<p id="tipo-equipamento"style="display: none">${equipamento.tipo}</p>
                               <div style="height: 75%; overflow: hidden; display: flex; justify-content:center"><img style="width: 100%; height: 80%; object-fit: contain; padding: .5vw" alt="imgProduto" id="imgProduto" src="src/images/${equipamento.tipo}.png"/></div>
                               <div id="texto-equipamento"><p>${equipamento.nome}</p>
-                              <p>Status: ${equipamento.status}</p>
+                              <p style="color: green"><strong>${equipamento.status}<strong></p>
                               <p>R$ ${equipamento.preco}</p></div>`;
         container.appendChild(elemento);
     });
