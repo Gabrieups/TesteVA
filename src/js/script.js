a2d257d073b6d04841e118f8cfcd913dca0dc817
Gabriel Souza <gabriel.souza@servier.com>
2024-04-08 13:45:30 -0300

    czcascasdc

--- src/js/script.js
+++ src/js/script.js
@@ -10,8 +10,9 @@ function mostrarEquipamentos() {
     container.innerHTML = "";
 
     equipamentos.forEach(equipamento => {
-        const elemento = document.createElement("div");
+        const elemento = document.createElement("a");
         elemento.id = 'equipamentosId'
+        elemento.href = '#'
         elemento.innerHTML = `<p id="tipo-equipamento"style="display: none">${equipamento.tipo}</p>
                               <div style="height: 75%; overflow: hidden; display: flex; justify-content:center"><img style="width: 100%; height: 80%; object-fit: contain; padding: .5vw" alt="imgProduto" id="imgProduto" src="src/images/${equipamento.tipo}.png"/></div>
                               <div id="texto-equipamento"><p>${equipamento.nome}</p>
