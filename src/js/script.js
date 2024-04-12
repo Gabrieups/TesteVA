4885acfeb52d6839ecd926bb5b76a153dd6f92ca
Gabriel Souza <gabriel.souza@servier.com>
2024-04-12 16:15:08 -0300

    zdsgdsg

--- src/js/script.js
+++ src/js/script.js
@@ -8,9 +8,11 @@ function mostrarEquipamentos() {
         elemento.href = '#'
         elemento.innerHTML = `<p id="tipo-equipamento"style="display: none">${equipamento.tipo}</p>
                               <div style="height: 75%; overflow: hidden; display: flex; justify-content:center"><img style="width: 100%; height: 80%; object-fit: contain; padding: .5vw" alt="imgProduto" id="imgProduto" src="src/images/${equipamento.tipo}.png"/></div>
-                              <div id="texto-equipamento"><p>${equipamento.nome}</p>
-                              <p style="color: green"><strong>${equipamento.estado}<strong></p>
-                              <p>R$ ${equipamento.preco}</p></div>`;
+                              <div id="texto-equipamento">
+                                <p style="font-size: .9vw; margin: 0;">${equipamento.nome}</p>
+                                <p style="color: #f55b41; font-size: 1.5vw; margin: 0;">R$ ${equipamento.preco}</p>
+                                <p style="font-size: .9vw; margin: 0;"><strong>${equipamento.estado}<strong></p>
+                              </div>`;
         container.appendChild(elemento);
     });
 }
@@ -47,11 +49,11 @@ function btnSalvarAlerta(){
 
     if(select == "S"){
         if (imgAlerta.src.includes("sino.svg")){
-            imgAlerta.src = "src/images/sino_cortado.svg";
+            imgAlerta.src = "/src/images/sino_cortado.svg";
             txt.textContent = "Deseja remover o alerta de produtos disponíveis para a compra ?";
             imgClose();
         } else {
-            imgAlerta.src = "src/images/sino.svg";
+            imgAlerta.src = "/src/images/sino.svg";
             txt.textContent = "Deseja ativar o alerta de produtos disponíveis para a compra ?";
             imgClose();
         }
