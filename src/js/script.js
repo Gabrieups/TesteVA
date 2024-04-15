7f190a3a5e11fbc58ce58d4d42b3c996c957412b
Gabriel Souza <gabriel.souza@servier.com>
2024-04-15 08:34:00 -0300

    sadasda

--- src/js/script.js
+++ src/js/script.js
@@ -7,7 +7,7 @@ function mostrarEquipamentos() {
         elemento.id = 'equipamentosId'
         elemento.href = '#'
         elemento.innerHTML = `<p id="tipo-equipamento"style="display: none">${equipamento.tipo}</p>
-                              <div style="height: 75%; overflow: hidden; display: flex; justify-content:center"><img style="width: 100%; height: 80%; object-fit: contain; padding: .5vw" alt="imgProduto" id="imgProduto" src="src/images/${equipamento.tipo}.png"/></div>
+                              <div style="height: 75%; overflow: hidden; display: flex; justify-content:center"><img style="width: 100%; height: 80%; object-fit: contain; padding: .5vw" alt="imgProduto" id="imgProduto" src="../images/${equipamento.tipo}.png"/></div>
                               <div id="texto-equipamento">
                                 <p style="font-size: .9vw; margin: 0;">${equipamento.nome}</p>
                                 <p style="color: #f55b41; font-size: 1.5vw; margin: 0;">R$ ${equipamento.preco}</p>
